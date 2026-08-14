57526c72a5ecf388ec0aa4af0fac4e339d955d46
payal <payalmalakar55@gmail.com>
2026-08-14 17:04:58 +0530

    add

--- trioaas-website/src/lib/site-contact.ts
+++ trioaas-website/src/lib/site-contact.ts
@@ -6,7 +6,7 @@ export const CONTACT_API_URL = _envUrl
   ? _envUrl.includes("/api")
     ? _envUrl.replace(/\/$/, "")
     : _envUrl.replace(/\/$/, "") + "/api/contact"
-  : "https://trionexa-1.onrender.com/api/contact";
+  : "https://trionexa-2.onrender.com/api/contact";
 
 export type ContactForm = {
   name: string;
